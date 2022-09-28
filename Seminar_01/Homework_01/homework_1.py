f9833754c3b21ec22767dd7ef63d3623114240eb
@igor_mashkin <igor-190677@yandex.ru>
2022-09-28 12:06:57 +0300

    Add change homework_1.py in Seminar_01

--- Seminar_01/Homework_01/homework_1.py
+++ Seminar_01/Homework_01/homework_1.py
@@ -4,9 +4,24 @@
 # - 7 -> да
 # - 1 -> нет
 
+def input_number():
+    day = False
+    while day == False:
+        try:
+            numb_day = int(input('Ведите номер дня недели: '))
+            day = True
+        except ValueError:
+            print("Это не число!")
+        if day == True and (numb_day <= 0 or numb_day >= 8):
+            print('Число должно быть в диапазоне от 1 до 7!')
+            day = False
+    return numb_day
 
-number_day = int(input('Ведите номер дня недели: '))
-if number_day == 6 or number_day == 7:
-    print('День является выходным')
-else:
-    print('День не является выходным') 
+def check_number_day(number_day):
+    if number_day == 6 or number_day == 7:
+        print('День является выходным')
+    else:
+        print('День не является выходным') 
+
+number_day = input_number()
+check_number_day(number_day)
