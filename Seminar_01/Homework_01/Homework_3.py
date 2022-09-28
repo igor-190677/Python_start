208f5e83248d7aee090ae2844d2c357866685fc2
@igor_mashkin <igor-190677@yandex.ru>
2022-09-28 18:12:46 +0300

    Add homework_3.py in Seminar_01

--- Seminar_01/Homework_01/Homework_3.py
+++ Seminar_01/Homework_01/Homework_3.py
@@ -6,3 +6,30 @@
 # - x=2; y=4-> 1
 # - x=-34; y=-30 -> 3
 
+def input_coordinate():
+    flag = False
+    while flag == False:
+        try:
+            coordinate = int(input())
+            flag = True
+        except ValueError:
+            print("Это не число!")
+    return coordinate
+
+def definition_quarter(coord_x, coord_y):
+    if coord_x > 0 and coord_y > 0:
+        quarter = 1
+    if coord_x < 0 and coord_y > 0:
+        quarter = 2
+    if coord_x < 0 and coord_y < 0:
+        quarter = 3
+    if coord_x > 0 and coord_y < 0:
+        quarter = 4        
+    print('Точка находится в четверти', quarter)
+
+
+print('Введите координату Х: ',end='')
+coordinate_x = input_coordinate()
+print('Введите координату Y: ',end='')
+coordinate_y = input_coordinate()
+definition_quarter(coordinate_x, coordinate_y)
